69fe5aa3f11c919007e93a5370ae6b5a4a363723
nagarajkm <93265961+nagarajkm@users.noreply.github.com>
2022-03-27 22:32:16 +0530

    lab

---
+++ StudentDebateMgmt/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,52 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name="student")
+@Data
+@AllArgsConstructor
+public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+
+	@Column(name="firstName")
+	private String firstName;
+
+
+	@Column(name="lastName")
+	private String lastName;
+
+
+	@Column(name="course")
+	private String course;
+	
+	@Column(name="country")
+	private String country;
+
+	public Student() {
+		super();
+		
+	}
+
+	public Student(String firstName, String lastName, String course, String country) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.course = course;
+		this.country = country;
+	}
+	
+	
+}
---
+++ StudentDebateMgmt/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.example.demo.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+	
+	//List<Student> findByNameContainsAndAuthorContainsAllIgnoreCase(String firstName,String lastName);
+}
